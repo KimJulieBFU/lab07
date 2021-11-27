e51cf2e024668c3ff63a4c0b3c6effaedc90ab27
KimJulieBFU <kimjulie@vk.com>
2021-11-27 22:10:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-def rot(num):
-    str_num = str(num)  # перевод числа в строку для сравненения
-    length = len(str_num)  # количество разрядов числа
-    for i in range(0, length):
-        if str_num[i] == '6':
-            num = num - 6 * (10 ** (length - i - 1)) + 9 * (10 ** (length - i - 1))
-            return num
-    return num
+def merge(*lists):
+    new_line = [] # формируем новый список
+    temporary_list = []
+
+    for i in range(len(lists[0]) - 1, -1, -1):  # объединяем списки
+        temporary_list = temporary_list + lists[0][i]
+
+    while temporary_list:
+        sort_num = min(temporary_list) # ищем минимальные значения в списке
+        while sort_num in temporary_list: # добавляем минимальное значение в новый список
+            new_line.append(sort_num)
+            temporary_list.remove(sort_num)
+            
+    return new_line
